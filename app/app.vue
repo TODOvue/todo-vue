471d7f94efbf431d9ffeabe2bcc9adae1bd0cabc
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-13 16:54:24 -0500

    feat(app): add blog menu component and integrate blog data fetching

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { TvThemeButton } from '@todovue/tv-theme-button'
+import {TvThemeButton} from '@todovue/tv-theme-button'
+import {TvMenu} from '@todovue/tv-menu'
+
+const router = useRouter()
 
 const setTheme = (value) => {
   document.documentElement.className = value + '-mode'
@@ -10,6 +13,44 @@ const changeValue = (value) => {
   setTheme(value)
 }
 
+const { data: posts } = await useAsyncData('blog-list', async () => {
+  try {
+    return await queryCollection('blog').all()
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+})
+
+const results = posts.value.map(post => ({
+  title: post.title,
+  url: post.path,
+  id: post.id,
+}))
+
+const configMenu = {
+  menus: [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Blogs",
+      url: "/blog",
+    },
+  ],
+  placeholder: "Search blogs...",
+  titleButton: "Search",
+  imageMenu: "https://firebasestorage.googleapis.com/v0/b/todovue-blog.appspot.com/o/logo.png?alt=media&token=4d64783f-2259-49cc-a6b4-68e58ce3b227",
+  results
+};
+
+const handleClickMenu = (menu) => {
+  if (typeof menu === 'string') return
+  router.push(menu.url)
+}
 onMounted(() => {
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
   const savedTheme = localStorage.getItem('theme')
@@ -20,10 +61,19 @@ onMounted(() => {
 </script>
 
 <template>
+  <TvMenu
+    :menus="configMenu.menus"
+    :placeholder="configMenu.placeholder"
+    :title-button="configMenu.titleButton"
+    :image-menu="configMenu.imageMenu"
+    :results="configMenu.results"
+    @click-menu="handleClickMenu"
+    @search-menu="handleClickMenu"
+  />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <tv-theme-button @change-theme="changeValue"/>
+  <TvThemeButton @change-theme="changeValue"/>
 </template>
 
 <style>
