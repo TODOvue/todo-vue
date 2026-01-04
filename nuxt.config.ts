c4c4b66fefc1497c9d9adb1b7af7fc894fc3caad
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-03 19:50:09 -0500

    feat(nuxt.config): update favicon links and add apple mobile web app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,9 +27,14 @@ export default defineNuxtConfig({
         { name: 'theme-color', content: '#42b883' },
         { name: 'msapplication-TileColor', content: '#42b883' },
         { name: 'msapplication-TileImage', content: '/favicon.ico' },
+        { name: 'apple-mobile-web-app-title', content: 'TODOvue' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
       ]
     }
   },
