d3686140bf3d700cbb106fb9cda156b059626dc0
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-09 21:47:59 -0500

    feat(.htaccess, deploy.yml, deploy_develop.yml, nuxt.config.ts): enhance Firebase configuration and add measurement ID support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,8 @@ export default defineNuxtConfig({
         storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
-        databaseURL: process.env.NUXT_PUBLIC_FIREBASE_DATABASE_URL
+        databaseURL: process.env.NUXT_PUBLIC_FIREBASE_DATABASE_URL,
+        measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID
       }
     }
   },
