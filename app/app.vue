b8f7171e0b66464242d6fa52d148bcc132213001
Cristhian Daza <cristian.d.b@hotmail.com>
2026-01-11 18:22:57 -0500

    feat(todo-vue): update pnpm version to 10.28.0 and increment app version to 0.1.6

--- app/app.vue
+++ app/app.vue
@@ -192,7 +192,7 @@ const configFooter = computed(() => ({
       }))
     },
   ],
-  version: '0.1.5',
+  version: '0.1.6',
   legal: [
     { label: 'TODOvue UI', url: 'https://ui.todovue.blog', },
     { label: 'CrisDev', url: 'https://cris-dev.com', },
